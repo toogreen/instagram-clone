57e24b1499f7f3050354f163ebf853b65673b860
David Gagnon <toogreen@15inch.lan>
2020-08-19 21:52:21 -0400

    added Instagram logo in Header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Hello World</h1>
+
+     <header className="app__header">
+      <img 
+        className="app__headerImage"
+        src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+      />
+
+     </header>
+
+      {/* { Header } */}
+      
+      {/* { Posts } */}
+
     </div>
   );
 }
