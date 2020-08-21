a212007fc2f032338cede14786ce4fceb977a5a1
David Gagnon <toogreen@15inch.lan>
2020-08-21 10:12:14 -0400

    attempt at loading MP4 videos (may revert)

--- src/Post.js
+++ src/Post.js
@@ -3,6 +3,10 @@ import "./Post.css"
 import Avatar from "@material-ui/core/Avatar"
 
 function Post({username, caption, imageUrl}) {
+    
+        //alert(imageUrl.split('.').pop())
+        
+
     return (
         <div className="post">
             <div className="post__header">
@@ -16,7 +20,16 @@ function Post({username, caption, imageUrl}) {
  
             {/* Header -> avatar + username  */}
 
-            <img className="post__image" src={imageUrl} alt="" />
+
+            {(imageUrl.split('.').pop() === '.mp4*') ? (
+            <video>
+                <source src={imageUrl} type='video/mp4' />
+                Your browser does not support the video tag.
+            </video>
+            ): (
+                <img className="post__image" src={imageUrl} alt="" />
+            )}
+            
 
 
             <h4 className="post__text"><strong>{username}: </strong>{caption}</h4>
